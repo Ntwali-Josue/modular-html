16e94acc326e83dd4e3254df0cb5862009ade532
Ntwali-Josue <kntwalijos@gmail.com>
2023-08-16 09:21:25 +0200

    sample tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./sections/**/*.{html}, index.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
